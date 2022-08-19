0e4aad3f496bbfb13b25023af4ecfb946246f41a
mohammadreza-moghadam <mohammadreza.che.eng@gmail.com>
2022-08-19 15:10:38 +0430

    adding authorization with jwt and creating middleware

--- routes/auth.js
+++ routes/auth.js
@@ -2,18 +2,14 @@ const express = require("express")
 const router = express.Router()
 const User = require("../models/User")
 const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 const validateRegisterInput = require("../validation/registerValidation")
+const requiresAuth = require("../middleware/permissions");
 
-// @route GET api/auth/test
-// @desc Test the auth route
-// @access Public
 router.get("/test", (req, res) => {
     res.send("Auth route working")
 })
 
-// @route POST api/auth/register
-// @desc Create a new user
-// @access Public
 router.post("/register", async (req, res) => {
     try {
         const {errors, isValid} = validateRegisterInput(req.body)
@@ -41,12 +37,62 @@ router.post("/register", async (req, res) => {
         // save user to the database
         const savedUser = await newUser.save()
 
+        const userToReturn = {...savedUser._doc}
+        delete userToReturn.password
+
         // return the new user
-        return res.json(savedUser)
+        return res.json(userToReturn)
     } catch (err){
         console.log(err)
-        res.status(500).send(err.message)
+        return res.status(500).send(err.message)
     }
+})
+
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({
+            email: new RegExp("^" + req.body.email + "$" , "i")
+        })
+        if (!user) {
+            return res.status(400).json({error: "There was a problem with your login credentials"})
+        }
+
+        const passwordMatch = await bcrypt.compare(req.body.password, user.password)
+
+        if (!passwordMatch) {
+            return res.status(400).json({error: "There was a problem with your login credentials"})
+        }
+
+        const payload = {userId: user._id}
+        const token=  jwt.sign(payload, process.env.JWT_SECRET, {
+            expiresIn: "7d"
+        })
+
+        res.cookie("access-token", token, {
+            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production"
+        })
+
+        const userToReturn = {...user._doc}
+        delete userToReturn.password
+
+        return res.json({
+            token: token,
+            user: userToReturn
+        })
+    } catch (err) {
+        console.log(err)
+        return res.status(500).send(err.message)
+    }
+})
+
+router.get("/current", requiresAuth, (req, res) => {
+    if(!req.user) {
+        return res.status(401).send("Unauthorized")
+    }
+
+    return res.json(req.user)
 })
 
 module.exports = router
