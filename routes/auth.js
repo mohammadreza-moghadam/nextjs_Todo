55e452002d2f16bd45fd0ad4364957986f689520
mohammadreza-moghadam <mohammadreza.che.eng@gmail.com>
2022-08-19 18:02:17 +0430

    refactor authjs

--- routes/auth.js
+++ routes/auth.js
@@ -1,95 +1,125 @@
-const express = require("express")
-const router = express.Router()
-const User = require("../models/User")
-const bcrypt = require("bcryptjs")
-const jwt = require("jsonwebtoken")
-const validateRegisterInput = require("../validation/registerValidation")
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const validateRegisterInput = require("../validation/registerValidation");
+const jwt = require("jsonwebtoken");
 const requiresAuth = require("../middleware/permissions");
 
 router.post("/register", async (req, res) => {
     try {
-        const {errors, isValid} = validateRegisterInput(req.body)
+        const { errors, isValid } = validateRegisterInput(req.body);
 
         if (!isValid) {
-            return res.status(400).json(errors)
+            return res.status(400).json(errors);
         }
+
         // check for existing user
         const existingEmail = await User.findOne({
-            email: new RegExp("^" + req.body.email + "$", "i")
-        })
+            email: new RegExp("^" + req.body.email + "$", "i"),
+        });
 
         if (existingEmail) {
-            return res.status(400).json({error: "User already exists with this email"})
+            return res
+                .status(400)
+                .json({ error: "There is already a user with this email" });
         }
 
-        // hashing the password
-        const hashPassword = await bcrypt.hash(req.body.password, 12)
+        // hash the password
+        const hashedPassword = await bcrypt.hash(req.body.password, 12);
+
+        // create a new user
         const newUser = new User({
             email: req.body.email,
-            password: hashPassword,
-            name: req.body.name
-        })
+            password: hashedPassword,
+            name: req.body.name,
+        });
 
-        // save user to the database
-        const savedUser = await newUser.save()
+        // save the user to the database
+        const savedUser = await newUser.save();
 
-        const userToReturn = {...savedUser._doc}
-        delete userToReturn.password
+        const payload = { userId: savedUser._id };
 
-        // return the new user
-        return res.json(userToReturn)
-    } catch (err){
-        console.log(err)
-        return res.status(500).send(err.message)
-    }
-})
-
-router.post("/login", async (req, res) => {
-    try {
-        const user = await User.findOne({
-            email: new RegExp("^" + req.body.email + "$" , "i")
-        })
-        if (!user) {
-            return res.status(400).json({error: "There was a problem with your login credentials"})
-        }
-
-        const passwordMatch = await bcrypt.compare(req.body.password, user.password)
-
-        if (!passwordMatch) {
-            return res.status(400).json({error: "There was a problem with your login credentials"})
-        }
-
-        const payload = {userId: user._id}
-        const token=  jwt.sign(payload, process.env.JWT_SECRET, {
-            expiresIn: "7d"
-        })
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+            expiresIn: "7d",
+        });
 
         res.cookie("access-token", token, {
             expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production"
-        })
+            secure: process.env.NODE_ENV === "production",
+        });
 
-        const userToReturn = {...user._doc}
-        delete userToReturn.password
+        const userToReturn = { ...savedUser._doc };
+        delete userToReturn.password;
+
+        // return the new user
+        return res.json(userToReturn);
+    } catch (err) {
+        // error here
+        console.log(err);
+
+        res.status(500).send(err.message);
+    }
+});
+
+router.post("/login", async (req, res) => {
+    try {
+        // check for the user
+        const user = await User.findOne({
+            email: new RegExp("^" + req.body.email + "$", "i"),
+        });
+
+        if (!user) {
+            return res
+                .status(400)
+                .json({ error: "There was a problem with your login credentials" });
+        }
+
+        const passwordMatch = await bcrypt.compare(
+            req.body.password,
+            user.password
+        );
+
+        if (!passwordMatch) {
+            return res
+                .status(400)
+                .json({ error: "There was a problem with your login credentials" });
+        }
+
+        const payload = { userId: user._id };
+
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+            expiresIn: "7d",
+        });
+
+        res.cookie("access-token", token, {
+            expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000),
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+        });
+
+        const userToReturn = { ...user._doc };
+        delete userToReturn.password;
 
         return res.json({
             token: token,
-            user: userToReturn
-        })
+            user: userToReturn,
+        });
     } catch (err) {
-        console.log(err)
-        return res.status(500).send(err.message)
+        console.log(err);
+
+        return res.status(500).send(err.message);
     }
-})
+});
 
 router.get("/current", requiresAuth, (req, res) => {
-    if(!req.user) {
-        return res.status(401).send("Unauthorized")
+    if (!req.user) {
+        return res.status(401).send("Unauthorized");
     }
 
-    return res.json(req.user)
-})
+    return res.json(req.user);
+});
 
 router.put("/logout", requiresAuth, async (req, res) => {
     try {
@@ -102,4 +132,4 @@ router.put("/logout", requiresAuth, async (req, res) => {
     }
 });
 
-module.exports = router
+module.exports = router;
